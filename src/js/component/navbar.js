dd748c042099fbf0f76391648eca448eb5e0dc72
efuentes85 <efuentes85@gmail.com>
2019-11-26 23:48:05 -0300

    rollback de navbar y login

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,50 +1,66 @@
 import React, { Component } from "react";
-import { Nav, Container, Row } from "react-bootstrap";
-import PropTypes from "prop-types";
-
+import Popup from "reactjs-popup";
+import {
+  MDBBtn,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBFormInline,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem
+} from "mdbreact";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Login } from "./login";
 
 export class Navbar extends Component {
-	state = {
-		isOpen: false
-	};
+  state = {
+    isOpen: false
+  };
 
-	toggleCollapse = () => {
-		this.setState({ isOpen: !this.state.isOpen });
-	};
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
-	render() {
-		return (
-			<Container>
-				<Nav
-					className="justify-content-between navbar navbar-dark bg-dark navbar-expand-lg fixed-top"
-					defaultActiveKey="/home"
-					as="ul"
-					variant="primary">
-					<Nav.Item className="navbar-brand">
-						<img src="./src/img/logo_lion.jpg" />
-					</Nav.Item>
-					<Nav.Item as="li">
-						<Nav.Link href="/home">Active</Nav.Link>
-					</Nav.Item>
-					<Nav.Item as="li">
-						<Nav.Link eventKey="link-1">Link1</Nav.Link>
-					</Nav.Item>
-					<Nav.Item as="li">
-						<Nav.Link eventKey="link-2">Link2</Nav.Link>
-					</Nav.Item>
-					<Nav.Item as="li">
-						<Nav.Link eventKey="link-3">Link3</Nav.Link>
-					</Nav.Item>
-					<Nav.Item as="li" className="justify-content-end">
-						{" "}
-						<Login />
-						<Nav.Item></Nav.Item>
-					</Nav.Item>
-				</Nav>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <MDBNavbar color="indigo" dark expand="md">
+        <MDBNavLink to="/">
+          <MDBNavbarBrand>
+            <strong className="white-text">eTeams</strong>
+          </MDBNavbarBrand>
+        </MDBNavLink>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+              <MDBNavLink to="/">
+                <a className="nav-link">Home</a>
+              </MDBNavLink>
+            </MDBNavItem>
+
+            <MDBNavItem>
+              <MDBNavLink to="/players">
+                <a className="nav-link">Gamers</a>
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/teams">
+                <a className="nav-link">Teams</a>
+              </MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+            <Login />
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
 }
